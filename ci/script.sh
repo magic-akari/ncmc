ffadbe9d4c26ee3df366f09ede8db47a79555eae
阿卡琳 <hufan.akarin@Gmail.com>
2018-08-16 15:22:18 +0800

    fix ci

--- ci/script.sh
+++ ci/script.sh
@@ -10,8 +10,8 @@ main() {
         return
     fi
 
-    # cross test --target $TARGET
-    # cross test --target $TARGET --release
+    cross test --target $TARGET
+    cross test --target $TARGET --release
 
     cross run --target $TARGET
     cross run --target $TARGET --release
